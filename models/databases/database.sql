b48dfb4b81db0b2e2b5b66c3ef288d3e314db4ea
x6galixia <147425442+x6galixia@users.noreply.github.com>
2024-07-04 08:31:04 +0800

    edited values

--- models/databases/database.sql
+++ models/databases/database.sql
@@ -10,21 +10,24 @@ CREATE TABLE users (
 	user_type VARCHAR(200) NOT NULL
 );
 -- Create table
-CREATE TABLE inventory (
-	item_name VARCHAR(250) NOT NULL,
-	brand VARCHAR(250) NOT NULL,
-	expiration DATE NOT NULL,
-	quantity INT NOT NULL,
-	PRIMARY KEY (item_name, brand)
-);
+CREATE TABLE inventory
+(
+    item_name VARCHAR(250) NOT NULL,
+    brand VARCHAR(250) NOT NULL,
+    manufacturer VARCHAR(250) NOT NULL,
+    dosage VARCHAR(250) NOT NULL,
+    expiration DATE NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (item_name, brand)
+)
 -- Inserting additional data into users table
 INSERT INTO users (name, username, password, user_type)
 VALUES 
 	('Kim Galicia', 'milktea', '1234', 'pharmacist');
 
 -- Inserting data into inventory table
-INSERT INTO inventory (item_name, brand, expiration, quantity)
+INSERT INTO inventory (item_name, brand, manufacturer, dosage, expiration, quantity)
 VALUES
-	('Aspirin', 'Bayer', '2024-12-31', 100),
-	('Ibuprofen', 'Advil', '2025-01-15', 200);
+	('Aspirin', 'Bayer', 'hotdog', '25 mg', '2024-12-31', 100),
+	('Ibuprofen', 'Advil', 'hotdog', '25 mg', '2025-01-15', 200);
 	
